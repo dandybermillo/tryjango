47ee81712c8f854098721f49b238ee8add578411
dandybermillo <dandy bermillo>
2020-12-18 13:29:49 +0800

    12-18b

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '^@qq-@h+hiybzhouag7!jd#gj(!ngh7f0o9)%l@vqch23$4g94'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.104.206.78']
 
 
 # Application definition
