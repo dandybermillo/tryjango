2b1fbf78a98b1097d44e96d35ac0201dc3d64727
dandybermillo <dandy bermillo>
2020-12-18 20:40:15 +0800

    12-18-8b

--- trydjango/local_settings.py
+++ trydjango/local_settings.py
@@ -2,7 +2,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dandybermillo',
-        'USER': 'postgres',
+        'USER': 'dandybermillo',
         'PASSWORD': 'Ollimreb200',
         'HOST': 'localhost',
         'PORT': '5432',
